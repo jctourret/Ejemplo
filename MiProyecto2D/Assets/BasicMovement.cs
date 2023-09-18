77461fe5074c365cd2e9b986928a6216cda1ef9d
jctourret <jctourret@gmail.com>
2023-09-18 12:32:22 -0300

    Nombre del comit

--- MiProyecto2D/Assets/BasicMovement.cs
+++ MiProyecto2D/Assets/BasicMovement.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class BasicMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    float horizantal;
+    float vertical;
 
+    public float walkingSpeed = 1;
     // Update is called once per frame
     void Update()
     {
-        
+        horizantal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+
+        Vector2 movimiento = ((Vector2.right*horizantal) + (Vector2.up*vertical)) * walkingSpeed;
+
+        // Transform == Componente / transform == variable
+        transform.position += new Vector3(movimiento.x,movimiento.y,0) * Time.deltaTime;
     }
 }
